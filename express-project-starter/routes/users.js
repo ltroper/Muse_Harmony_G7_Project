bc7a8ebfe6a70401749105eef6a4a42d595463bc
Nikolas <nikolas.garza5@gmail.com>
2022-02-15 08:58:10 -0800

    got server running

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,75 +1,78 @@
-const {csrfProtection, asyncHandler} = require('./util');
-const { loginUser, logoutUser } = require('../auth');
-const {check, validationResult} = require('express-validator');
-const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require("./util");
+const { loginUser, logoutUser } = require("../auth");
+const { check, validationResult } = require("express-validator");
+const db = require("../db/models");
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-
+const bcrypt = require("bcryptjs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/login', csrfProtection, (req, res) => {
-  res.render('user-login', {
-    title: 'Login',
-    csrfToken = req.csrfToken(),
+router.get("/login", csrfProtection, (req, res) => {
+  res.render("login", {
+    title: "Login",
+    csrfToken: req.csrfToken(),
   });
 });
 
 const loginValidators = [
-  check('email')
+  check("email")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for Email Address')
-    .isLength({max: 50})
-    .withMessage('Email must be less than 50 characters'),
-  check('password')
+    .withMessage("Please provide a value for Email Address")
+    .isLength({ max: 50 })
+    .withMessage("Email must be less than 50 characters"),
+  check("password")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for Password')
-    .isLength({max: 50})
-    .withMessage('Password must be less than 50 characters'),
+    .withMessage("Please provide a value for Password")
+    .isLength({ max: 50 })
+    .withMessage("Password must be less than 50 characters"),
 ];
 
-router.post('/login', csrfProtection,
-loginValidators, asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/login",
+  csrfProtection,
+  loginValidators,
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-  let errors = [];
+    let errors = [];
 
-  const validateErrors = validationResult(req);
+    const validateErrors = validationResult(req);
 
-  if(validateErrors.isEmpty()){
-    const user = await db.User.findOne({where: {email}});
+    if (validateErrors.isEmpty()) {
+      const user = await db.User.findOne({ where: { email } });
 
-    if (user !== null){
-      const passwordMatch = await bcrypt.compare(password, user.hashPassword.toString());
-      if (passwordMatch){
-        loginUser(req, res, user);
-        return res.redirect('./');
+      if (user !== null) {
+        const passwordMatch = await bcrypt.compare(
+          password,
+          user.hashPassword.toString()
+        );
+        if (passwordMatch) {
+          loginUser(req, res, user);
+          return res.redirect("./");
+        }
       }
+
+      errors.push("Login failed for the provided email address and password");
+    } else {
+      errors = validateErrors.array().map((error) => error.msg);
     }
 
-    errors.push('Login failed for the provided email address and password');
+    res.render("user-login", {
+      title: "Login",
+      email,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
 
-  }
-  else {
-    errors = validateErrors.array().map((error) => error.msg);
-  }
-
-  res.render('user-login', {
-    title: 'Login',
-    email,
-    errors,
-    csrfToken: req.csrfToken()
-  });
-}));
-
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   logoutUser(req, res);
-  res.redirect('/');
+  res.redirect("/");
 });
-
-
 
 module.exports = router;
--- express-project-starter/routes/util.js
+++ express-project-starter/routes/util.js
@@ -1,6 +1,5 @@
-const csrf = require('csurf')
-const csrfProtection = csrf({cookie: true})
-const asyncHandler = require('express-async-handler');
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
+const asyncHandler = require("express-async-handler");
 
-
-module.exports = {csrf, csrfProtection, asyncHandler}
+module.exports = { csrf, csrfProtection, asyncHandler };
