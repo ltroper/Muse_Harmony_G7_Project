2424a548a65c9ecf901d09b3f063ae3bfe99d1a8
Leon Troper <ltroper@gmail.com>
2022-02-17 18:52:59 -0600

    added albums page and albumsId page

---
+++ routes/albums.js
@@ -0,0 +1,39 @@
+const { csrfProtection, asyncHandler } = require("./util");
+const { check, validationResult } = require("express-validator");
+const db = require("../db/models");
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+
+
+router.get("/", async (req, res) => {
+    const albums = await db.Album.findAll()
+
+    res.render("albums", {albums})
+})
+
+
+router.get("/:id", asyncHandler (async (req, res) => {
+    const albumId = req.params.id;
+    const album = await db.Album.findOne({
+    where: { id: albumId },
+    });
+
+    const albumReviews = await db.Album.findOne({
+        where: {id: albumId},
+        include: {
+          model: db.User,
+          as: 'Reviews'
+        }
+      })
+
+    // console.log(albumReviews.Reviews[0].Review.content)
+
+    const review = albumReviews.Reviews[0].Review.content
+    res.render("albumPage", {album, review})
+
+}))
+
+
+module.exports = router;
