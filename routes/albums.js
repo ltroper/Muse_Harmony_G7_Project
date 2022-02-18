26cd5ae7c1819b8ea2b57692de32c84a4abebf40
Ian_Edwards <edwards.ian.c@gmail.com>
2022-02-18 06:00:16 -0600

    requireAuth added to albums

--- routes/albums.js
+++ routes/albums.js
@@ -1,5 +1,6 @@
 const { csrfProtection, asyncHandler } = require("./util");
 const { check, validationResult } = require("express-validator");
+const { loginUser, logoutUser, requireAuth} = require("../auth");
 const db = require("../db/models");
 const express = require("express");
 const router = express.Router();
@@ -7,14 +8,14 @@ const bcrypt = require("bcryptjs");
 
 
 
-router.get("/", async (req, res) => {
+router.get("/", requireAuth, async (req, res) => {
     const albums = await db.Album.findAll()
 
     res.render("albums", {albums})
 })
 
 
-router.get("/:id", asyncHandler (async (req, res) => {
+router.get("/:id", requireAuth, asyncHandler (async (req, res) => {
     const albumId = req.params.id;
     const album = await db.Album.findOne({
     where: { id: albumId },
