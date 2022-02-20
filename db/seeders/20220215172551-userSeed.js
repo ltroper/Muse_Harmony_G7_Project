5cde04e905830c52d1139e03efd2530d974e530c
Braxton Kappes <braxtonkappes@gmail.com>
2022-02-20 16:42:24 -0600

    css styling on the splashPage and profilePage

--- db/seeders/20220215172551-userSeed.js
+++ db/seeders/20220215172551-userSeed.js
@@ -17,7 +17,7 @@ module.exports = {
           email: "demouser1@demouser.com",
           hashPassword:"$2a$10$4I8LFMjdt6gQxN1ZRCYXJu7diSNa6AEUZD3YmhypAttkeM1QhWHRK",
           profilePicture: "http://placeimg.com/640/480/abstract",
-          biography: "test1",
+          biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
