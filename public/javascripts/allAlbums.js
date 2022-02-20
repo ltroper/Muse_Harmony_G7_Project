e44b9c0476ef4526570a865a4bea0e44bffbf8b1
Leon Troper <ltroper@gmail.com>
2022-02-20 16:34:59 -0600

    updated add to library button, now works

--- public/javascripts/allAlbums.js
+++ public/javascripts/allAlbums.js
@@ -23,30 +23,32 @@ Array.from(addToLibraryButtons).forEach((addButton) => {
 
         oneDropDown.addEventListener("change", async (e) => {
             const libraryName = e.target.value;
-            const res = await fetch(`/libraries/${libraryName}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                  },
-                body: JSON.stringify({
-                    name: libraryName,
-                    userId,
-                    albumId: addId
+                const res = await fetch(`/libraries/${libraryName}`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                      },
+                    body: JSON.stringify({
+                        name: libraryName,
+                        userId,
+                        albumId: addId
+                    })
                 })
+                const data = await res.json()
+                if (data){
+                    tableEle.className = "addToLibrary active"
+                    oneDropDown.className = "dropDown hidden"
+                    cancel.className = "cancel hidden"
+                    success.className = "success active"
+                    oneDropDown.value = ""
+                }
+                else {
+                    success.className = "success active"
+                    success.innerText = "Album already in library"
+                }
+            
+
             })
-            const data = await res.json()
-            if (data){
-                tableEle.className = "addToLibrary active"
-                oneDropDown.className = "dropDown hidden"
-                cancel.className = "cancel hidden"
-                success.className = "success active"
-                oneDropDown.value = ""
-            }
-            else {
-                success.className = "success active"
-                success.innerText = "Album already in library"
-            }
-        })
 
 
 
