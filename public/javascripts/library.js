4a3291360d7a63d95e2320f4562c0f39f62cd4a9
Leon Troper <ltroper@gmail.com>
2022-02-20 15:04:09 -0600

    added functionality to remove album button

--- public/javascripts/library.js
+++ public/javascripts/library.js
@@ -1,6 +1,7 @@
-const removeLibraryButton = document.getElementById("removeLibraryButton")
-const userId = document.getElementById("userId").innerHTML
-const libraryName = document.getElementById("libraryName").innerHTML
+const removeLibraryButton = document.getElementById("removeLibraryButton");
+const userId = document.getElementById("userId").innerHTML;
+const libraryName = document.getElementById("libraryName").innerHTML;
+const manyRemoveAlbum = document.getElementsByClassName("removeAlbum");
 
 
 removeLibraryButton.addEventListener("click", async (e) => {
@@ -14,4 +15,27 @@ removeLibraryButton.addEventListener("click", async (e) => {
     if (data){
         console.log("somethings right")
     }
+})
+
+Array.from(manyRemoveAlbum).forEach((removeButton) => {
+    removeButton.addEventListener("click", async (e) => {
+        const removeId = e.target.id.split("-")[1];
+        console.log(removeId)
+
+        const res = await fetch(`/libraries/${libraryName}/${removeId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+              },
+        })
+        const data = await res.json()
+        if (data){
+            document.getElementById(`album-${removeId}`).parentNode.parentNode.remove()
+        }
+
+
+
+        //await a fetch to destroy album with albumId and userId
+
+    })
 })
